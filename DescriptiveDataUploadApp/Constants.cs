610a4b2656ea0eddf0f79a4a4db5c39f4d558877
nikita-msft <104855063+nikita-msft@users.noreply.github.com>
2023-03-19 20:20:34 -0700

    Update Constants.cs

--- DescriptiveDataUploadApp/Constants.cs
+++ DescriptiveDataUploadApp/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string LoginBaseUrl = "https://login.microsoftonline.com";
         public const string NovaPrdUri = "9d827643-d003-4cca-9dc8-71213a8f1644";
+        public const string NovaPpeUri = "01f9d889-ee31-41cb-85fa-3ad7e0981fa1";
     }
 }
